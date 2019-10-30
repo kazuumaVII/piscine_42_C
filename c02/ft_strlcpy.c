cd3394ea80b0e50a0aebb8ebae0006c1e17207de
kazuumaVII <ffrances@student.42.fr>
2019-10-30 13:06:15 +0100

    finish

---
+++ c02/ft_strlcpy.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frfrance <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/30 13:05:41 by frfrance          #+#    #+#             */
+/*   Updated: 2019/10/30 13:05:50 by frfrance         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char			*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	ft_strncpy(dest, src, size);
+	return (ft_strlen(src));
+}
